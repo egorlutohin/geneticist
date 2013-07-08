d8da0d306603b13eea05b5590347942e5d140a14
Egor Lutohin <egor@lutohin.nsk.ru>
2013-07-08 17:53:56 +0700

    Начало нового интерфейса для кладра

--- urls.py
+++ urls.py
@@ -4,11 +4,13 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 
+from django.views.generic import TemplateView
+
 
 admin.autodiscover()
 
 
-js_info_dict = {'domain': 'djangojs', 'packages': ('django.conf', 'django.contrib.admin',), }
+#~ js_info_dict = {'domain': 'djangojs', 'packages': ('django.conf', 'django.contrib.admin',), }
 
 
 urlpatterns = patterns('',
@@ -17,10 +19,13 @@ urlpatterns = patterns('',
     url(r'^(?P<patient_id>\d+)/$', 'patient.views.edit', name='patient_edit'),
     url(r'^mkb\.json$', 'mkb.views.mkb', name='mkb'),
     url(r'^kladr.json$', 'kladr.views.kladr', name='kladr'),
-    (r'^admin/jsi18n/', 'django.views.i18n.javascript_catalog', js_info_dict),
+    url(r'^kladr/selectbox_demo/', TemplateView.as_view(template_name='kladr/selectbox.html')),
+    url(r'^kladr/typehead_demo/', TemplateView.as_view(template_name='kladr/typehead.html')),
+    #~ (r'^admin/jsi18n/', 'django.views.i18n.javascript_catalog', js_info_dict),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^%s$' % settings.LOGIN_URL[1:], 'django.contrib.auth.views.login', name='login'),
     url(r'^%s$' % settings.LOGOUT_URL[1:], 'django.contrib.auth.views.logout', {'template_name': 'registration/logout.html'}, name='logout',),
+
 )
 
 
