2bb2fc4aef31496ea8ca50195bc02690bb32e6bb
Egor Lutohin <egor@lutohin.nsk.ru>
2013-07-17 12:43:10 +0700

    little-fix

---
+++ checkmodules.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import django # djamgp
+import ldap # python-ldap
+import dbf # dbf
+import mptt # dajngo-mptt
+import memcache # python-memcache
+import widget_tweaks # django-widget-tweaks
+import django_history # django-history
+import MySQLdb
